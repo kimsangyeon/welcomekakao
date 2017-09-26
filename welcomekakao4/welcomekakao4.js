d3b3aa5adc22758f9e8b807a5d8b08c91c319b68
yeon <dho113@naver.com>
2017-09-26 23:18:10 +0900

    welcomkakao4
    - for문 으로 행에 1이 나올시 다음행에 1 검사하는 방식 시도

--- welcomekakao4/welcomekakao4.js
+++ welcomekakao4/welcomekakao4.js
@@ -1,5 +1,24 @@
 function solution(board)
 {
-    var answer = 1234;
+    var answer = 0;
+    var lowLength = board[0].length;
+    var colLength = board[0].length;
+    
+    var max = lowLength > colLength ? lowLength : colLength;
+    
+    var tmpBoard = board;
+    for(var i = 0; i < tmpBoard.length; i++) {
+        var count = 0;
+        for(var j = 0; j < tmpBoard[i].length; j++) {
+            if(tmpBoard[i][j] === 1) {
+                count++;
+            }
+        }
+        for(var k = j, nextIdx = i + 1; k < count; k++) {
+            
+        }
+    }
+    
+    
     return answer;
 }
