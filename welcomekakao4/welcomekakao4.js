2bcf24a0d02166b8c4267e41e1f3edfaf9e6519d
yeon <dho113@naver.com>
2017-09-28 00:03:21 +0900

    welcomekakao4
    - searchRow, searchCol, searchReverseRow, searchReverseCol function 탐색 함수 생

--- welcomekakao4/welcomekakao4.js
+++ welcomekakao4/welcomekakao4.js
@@ -1,6 +1,21 @@
-function solution(board)
-{
+function solution(board){
     var answer = 0;
     
     return answer;
+}
+
+function searchRow() {
+
+}
+
+function searchCol() {
+
+}
+
+function searchReverseRow() {
+
+}
+
+function searchReverseCol() {
+    
 }
