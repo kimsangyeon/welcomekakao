00a0a14a33f5ce48cf1b349775a85553bf1f4074
yeon <dho113@naver.com>
2017-10-16 23:08:04 +0900

    welcomekakao4
    - max num 초기값 -1

--- welcomekakao5/welcomekakao5.js
+++ welcomekakao5/welcomekakao5.js
@@ -1,22 +1,22 @@
 function solution(land) {
-	var answer = 0;
+    var answer = 0;
     var prevIndex = -1;
-    var maxNum = 0;
+    var maxNum = -1;
     
     for(var i = 0; i < land.length; i++) {
         var tmpIndex = 0;
         for(var j = 0; j < land[i].length; j++) {
             if(land[i][j] > maxNum && j !== prevIndex) {
                 maxNum = land[i][j];
-                land[i][tmpIndex] = maxNum;
+                land[i][tmpIndex] = 0;
                 land[i][j] = 'x';
                 tmpIndex = j;
             }
         }
         answer += maxNum;
         prevIndex = land[i].indexOf('x');
-        maxNum = 0;
+        maxNum = -1;
     }
 
-	return answer;
+    return answer;
 }
