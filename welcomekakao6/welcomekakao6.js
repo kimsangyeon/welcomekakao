b46fc66dba6182bb759007b2ff6a481291cb504f
yeon <dho113@naver.com>
2017-10-24 23:05:03 +0900

    welcomekakao6
    - index + 2에서부터 3개의 나열된 숫자를 비교하여 큰 index의 숫자 더하기

--- welcomekakao6/welcomekakao6.js
+++ welcomekakao6/welcomekakao6.js
@@ -1,4 +1,40 @@
 function solution(sticker) {
-	var answer = 36;
+	var answer = 0;
+    var length = sticker.length;
+    var sum, max = 0;
+    for(var i = 0; i < 3; i++) {
+        sum = sticker[i];
+        for(var j = i + 2; j < length; j += 2) {
+            if(i === 0 && j === length - 3) {
+                sum += sticker[j];
+                break;
+            }
+            if(i !== 0 && j === length - 1) {
+                sum += sticker[j];
+                break;
+            }
+            if(j === length - 2) {
+                if(sticker[j] > sticker[j + 1]) {
+                    sum += sticker[j];
+                } else {
+                    sum += sticker[j + 1];
+                }
+                break;
+            }
+            
+            if(sticker[j] + sticker[j + 2] > sticker[j + 1]) {
+                sum += (sticker[j] + sticker[j + 2]);
+                j += 2;
+            } else {
+                sum += sticker[j + 1];
+                j += 1;
+            }
+        }
+        if(sum > max) {
+            max = sum;
+        }
+    }
+    answer = max;
+    
 	return answer;
 }
